a646d245bf5ecbe166bb829fb93444ee351c5b46
Alvaro Reguera <alvaro.reguera@gmail.com>
2018-07-31 16:40:13 +0200

    primera versión

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const logger       = require('morgan');
 const path         = require('path');
 
 
-mongoose.Promise = Promise;
-mongoose
-  .connect('mongodb://localhost/trainep-project', {useMongoClient: true})
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+// mongoose.Promise = Promise;
+// mongoose
+//   .connect('mongodb://localhost/trainep-project', {useMongoClient: true})
+//   .then(() => {
+//     console.log('Connected to Mongo!')
+//   }).catch(err => {
+//     console.error('Error connecting to mongo', err)
+//   });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/project1', (req, res, next) => {
+  res.render('project1');
+});
+
+router.get('/project2', (req, res, next) => {
+  res.render('project2');
+});
+
+router.get('/project3', (req, res, next) => {
+  res.render('project3');
+});
+
+router.get('/project4', (req, res, next) => {
+  res.render('project4');
+});
+
+router.get('/project5', (req, res, next) => {
+  res.render('project5');
+});
+
+router.get('/project6', (req, res, next) => {
+  res.render('project6');
+});
+
+
+
+router.get('/?referencias', (req, res, next) => {
+  res.render('index');
+});
+
 module.exports = router;
